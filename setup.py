152889eef102f61af6c934f0aec85e6790e0355a
Gonz Saavedra <gonzalosaavedra@gmail.com>
2013-12-07 16:03:00 -0200

    Basic package structure.

---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+tzdb
+----
+
+tzdb provides sqlalchemy models, import and update utils for the timezone
+database data provided by timezonedb.com.
+
+Links
+`````
+
+* `Source code <http://github.com/gonz/python-tzdb/>`_
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='tzdb',
+    version='0.1-dev',
+    url='http://github.com/gonz/python-tzdb/',
+    license='MIT',
+    author='Gonzalo Saavedra',
+    author_email='gonz@talduken.com',
+    description='A python/sqlalchemy wrapper for the timezone '
+                'database (timezonedb.com).',
+    long_description=__doc__,
+    packages=['tzdb'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'SQLAlchemy==0.8.3',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Database',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
+)
